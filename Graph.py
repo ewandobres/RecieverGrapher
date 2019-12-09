f0a2832fd4a28f7482aefa02f45f8af21ee00fd9
Ewan Dobres <ewandobres@gmail.com>
2019-12-09 18:25:24 +0000

    x axis values track time

--- Graph.py
+++ Graph.py
@@ -19,6 +19,7 @@ choice = config.get('SETTINGS', 'Input Mode')
 sensornum = int(config.get('SETTINGS', 'Number of graphs'))
 portName = 'COM11'
 baudrate = 9600
+runOnce = 0
 curve = []
 ### START QtApp #####
 app = QtGui.QApplication([])  # you MUST do this once (initialize things)
@@ -26,8 +27,9 @@ app = QtGui.QApplication([])  # you MUST do this once (initialize things)
 win = pg.GraphicsWindow(title="Signal from serial port")  # creates a window
 
 for x in range(sensornum):
-    p = win.addPlot(title=titles[x])  # creates empty space for the plot in the window
-    curve.append(p.plot())  # create an empty "plot" (a curve to plot)
+    createPlotSpace = win.addPlot(title=titles[x])  # creates empty space for the plot in the window
+    curve.append(createPlotSpace.plot())  # create an empty "plot" (a curve to plot)
+    curve[x].scale(delay, 1)
 
 windowWidth = 100  # width of the window displaying the curve
 
@@ -76,14 +78,12 @@ def update():
     outfile.close()
 
     for x in range(sensornum):
-        global curve, ptr, dataArray
+        global curve, ptr, dataArray, runOnce
 
         dataArray[x] = (np.append(dataArray[x], float(lineread[x])))
         dataArray[x] = np.delete(dataArray[x], 0)
-        plt = (time.time() - start_time) / delay - windowWidth  # update x position for displaying the curve
         curve[x].setData(dataArray[x])  # set the curve with this data
-        curve[x].setPos(plt, 0)  # set x position in the graph to 0
-
+        curve[x].setPos(currenttime() - windowWidth*delay, 0)
         QtGui.QApplication.processEvents()  # you MUST process the plot now
 
 
@@ -96,6 +96,10 @@ def every(delay, task):
         time.sleep(max(0, next_time - time.time()))
         task()
         next_time += (time.time() - next_time) // delay * delay + delay
+
+
+def currenttime():
+    return round(time.time() - start_time, 2)
 
 
 every(delay, update)
